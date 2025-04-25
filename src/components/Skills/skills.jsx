4c3470d7771a67c1d1797ab0b18e6cb00b5dc780
Philippe De Oliveira <philippe.deoliveira75@gmail.com>
2025-04-25 13:21:44 +0100

    maj skills

--- src/components/Skills/skills.jsx
+++ src/components/Skills/skills.jsx
@@ -1,39 +1,29 @@
 import './skills.scss'
+import { useEffect } from 'react'
 import { useTranslation } from "react-i18next"
-import { useEffect, useRef } from 'react'
-import { React, Vite, Github, Node, Docker, Redux, Sass } from '@assets/import'
+import { React as ReactLogo, Vite, Github, Node, Docker, Redux, Sass } from '@assets/import'
 
 function Skills() {
   const { t } = useTranslation("translation")
-  const containerRef = useRef(null)
 
   useEffect(() => {
-    const container = containerRef.current
-    let scrollAmount = 0
-    const speed = 0.2 // vitesse du défilement
-
-    const scroll = () => {
-      scrollAmount += speed
-      if (scrollAmount >= container.scrollWidth / 2) {
-        scrollAmount = 0 // réinitialisation du défilement lorsque l'on atteint la fin
-      }
-      container.scrollLeft = scrollAmount
-    }
-
-    const intervalId = setInterval(scroll, 10) // intervalle de mise à jour à chaque 10ms
-
-    return () => clearInterval(intervalId) // Nettoyage à l'arrêt du composant
+    // Clone l'élément pour créer un effet infini
+    const slide = document.querySelector(".logos-slide")
+    const copy = slide.cloneNode(true)
+    document.querySelector(".logos").appendChild(copy)
   }, [])
 
-  const logos = [React, Vite, Github, Node, Docker, Redux, Sass]
+  const logos = [ReactLogo, Vite, Github, Node, Docker, Redux, Sass]
 
   return (
-    <div className='skillsContainer'>
+    <div className="skillsContainer">
       <h2>{t('skills.title')}</h2>
-      <div className='carousel' ref={containerRef}>
-        {[...logos, ...logos].map((logo, i) => (
-          <img key={i} src={logo} alt={`logo-${i}`} />
-        ))}
+      <div className="logos">
+        <div className="logos-slide">
+          {logos.map((logo, i) => (
+            <img key={i} src={logo} alt={`logo-${i}`} />
+          ))}
+        </div>
       </div>
     </div>
   )
